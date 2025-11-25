12a151b0d9f6387362430588c178c22a335263c9
reypjbaliguat <reypjbaliguat@gmail.com>
2025-11-25 16:19:05 +0800

    feat: initialize vitest and mui

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+/// <reference types="vitest" />
+
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: "jsdom",
+    setupFiles: "./core/test/setup.ts",
+    globals: true,
+    css: true,
+  },
+});
