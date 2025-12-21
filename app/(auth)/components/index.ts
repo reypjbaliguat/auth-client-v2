4f25a408794e88907eaf9b93f223aa21ae689edb
reypjbaliguat <reypjbaliguat@gmail.com>
2025-12-22 02:54:58 +0800

    refactor(auth): remove unused AuthLeftColumn and AuthRightColumn components

--- app/(auth)/components/index.ts
+++ app/(auth)/components/index.ts
@@ -1,4 +1,2 @@
 export { default as AuthFormContainer } from './AuthFormContainer';
-export { default as AuthLeftColumn } from './AuthLeftColumn';
-export { default as AuthRightColumn } from './AuthRightColumn';
 export { default as OtpForm } from './OtpForm';
