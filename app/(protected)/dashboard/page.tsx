653d598905b8e2760fd5ad33b148dcf1ecceaa05
reypjbaliguat <reypjbaliguat@gmail.com>
2025-12-28 02:50:06 +0800

    feat(dashboard): add logout button to allow users to sign out

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,13 +1,22 @@
 'use client';
 
 import ProtectedRoute from '@/core/components/ProtectedRoute';
+import { setUnauthenticated } from '@/core/store/authSlice';
+import { useAppDispatch } from '@/core/store/hooks';
+import { Button } from '@mui/material';
 
 export default function DashboardPage() {
+	const dispatch = useAppDispatch();
+
 	return (
 		<ProtectedRoute>
 			<main style={{ padding: 24 }}>
 				<h1>Dashboard</h1>
 				<p>Only visible to authenticated users.</p>
+
+				<Button variant="contained" onClick={() => dispatch(setUnauthenticated())}>
+					Logout
+				</Button>
 			</main>
 		</ProtectedRoute>
 	);
